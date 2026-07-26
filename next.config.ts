59633bb665776093a7152799dec4d46229703148
AmitySereavo <amitysereavopersonal@gmail.com>
2026-07-26 01:10:35 -0500

    feat: configure output file tracing excludes for shared file routes

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,17 @@
-const nextConfig = {};
+const sharedFileRouteExcludes = [
+  "./.git/**/*",
+  "./.next/cache/**/*",
+  "./protected-media/**/*",
+  "./public/media/**/*",
+  "./private-downloads/**/*",
+  "./node_modules/.cache/**/*",
+];
+
+const nextConfig = {
+  outputFileTracingExcludes: {
+    "/api/account/identity-verification/file": sharedFileRouteExcludes,
+    "/api/downloads/[downloadkey]": sharedFileRouteExcludes,
+  },
+};
 
 export default nextConfig;
